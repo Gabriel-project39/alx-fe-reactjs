77f7581977991b0ec2a20c785ea8c07ae33589f9
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2026-01-12 23:06:29 +0300

    delete

--- alx-react-app/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
-      <p>&copy; 2025 My React App. All Rights Reserved.</p>
-        </footer>
-    );
-}
-
-export default Footer;
