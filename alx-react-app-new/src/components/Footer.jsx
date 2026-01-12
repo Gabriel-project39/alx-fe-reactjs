b1d5b10335e7250b9b9784811e58c6be5177196e
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2026-01-13 00:27:16 +0300

    header,UserProfile

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,13 +1,9 @@
 function Footer() {
-    return (
-        <div>
-            <footer>
-                <p>© 2023 City Lovers</p>
-            </footer>
-        </div>
-    );
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>&copy; 2025 My React App. All Rights Reserved.</p>
+    </footer>
+  );
 }
-
-
 
 export default Footer;
