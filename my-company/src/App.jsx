d3b088adb1b3593d0e8bb0ae3e7398c93c325d7b
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2026-02-12 14:38:10 +0300

    Build simple company website with React Router

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,3 +1,9 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +15,13 @@ function App() {
   return (
     <>
       <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
