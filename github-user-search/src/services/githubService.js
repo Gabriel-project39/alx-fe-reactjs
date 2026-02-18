d9fda4901b44a144e932f56631a8351148348a71
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2026-02-18 18:16:29 +0300

    app jsx

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
-// REQUIRED function for the task
-export const fetchUserData = async (username) => {
+export async function fetchUserData(username) {
   const response = await axios.get(`https://api.github.com/users/${username}`);
   return response.data;
-};
+}
+        {/* Form with input and button */}
 
-// Advanced search for multiple users
-export const advancedUserSearch = async (username, location, minRepos) => {
+        
+// src/services/githubService.js
+import axios from "axios";
+
+export async function fetchAdvancedUsers(username, location, minRepos) {
+  // Build query string
   let query = "";
-
   if (username) query += `${username} in:login `;
   if (location) query += `location:${location} `;
   if (minRepos) query += `repos:>=${minRepos}`;
 
-  const response = await axios.get(
-    `https://api.github.com/search/users?q=${encodeURIComponent(query)}`
-  );
-
-  return response.data;
-};
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+  return response.data; // contains { items: [...] }
+}
